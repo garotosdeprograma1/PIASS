863068ed335dc34aadf484875c89346bd463149c
garotosdeprograma1 <skibidiniggiss@gmail.com>
2026-08-21 10:31:01 -0300

    Add files via upload

---
+++ sql/consulta.sql
@@ -0,0 +1,11 @@
+SELECT
+cliente.nome AS 'Nome Cliente',
+servico.nome_servico AS 'Serviço Solicitado',
+os.preco_unitario AS 'Valor Total',
+funcionario.id AS 'Id Funcionário',
+funcionario.nome AS 'Nome Funcionário'
+FROM os
+INNER JOIN cliente ON os.id_cliente = cliente.id
+INNER JOIN servico ON os.id_servico = servico.id
+INNER JOIN funcionario ON os.id_funcionario = funcionario.id
+WHERE cliente.cpf = '167.869.259-01';
