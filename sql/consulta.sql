6d042598ab9267341f0115fed1c4be76fd0a6804
garotosdeprograma1 <skibidiniggiss@gmail.com>
2026-08-21 11:18:16 -0300

    Add files via upload

--- sql/consulta.sql
+++ sql/consulta.sql
@@ -1,3 +1,5 @@
+-- Consulta Especifica de Algum Dado
+
 SELECT
 cliente.nome AS 'Nome Cliente',
 servico.nome_servico AS 'Serviço Solicitado',
@@ -9,3 +11,33 @@ INNER JOIN cliente ON os.id_cliente = cliente.id
 INNER JOIN servico ON os.id_servico = servico.id
 INNER JOIN funcionario ON os.id_funcionario = funcionario.id
 WHERE cliente.cpf = '167.869.259-01';
+
+-- Consulta Especifica de Tabelas Distintas
+
+SELECT
+    cliente.nome AS 'Nome Cliente',
+    servico.nome_servico AS 'Seviço Solicitado',
+    os.preco_unitario AS 'Valor Total',
+    funcionario.id AS 'Id Funcionário',
+    funcionario.nome AS 'Nome Funcionário'
+FROM os
+inner JOIN cliente ON os.id_cliente = cliente.id
+inner JOIN servico ON os.id_servico = servico.id
+inner JOIN funcionario ON os.id_funcionario = funcionario.id;
+
+-- Consulta Pela Primeira Letra/Número (CPF)
+
+SELECT
+cliente.nome AS 'Nome Cliente',
+servico.nome_servico AS 'Serviço Solicitado',
+os.preco_unitario AS 'Valor Total',
+funcionario.id AS 'Id Funcionário',
+funcionario.nome AS 'Nome Funcionário'
+FROM os
+INNER JOIN cliente ON os.id_cliente = cliente.id
+INNER JOIN servico ON os.id_servico = servico.id
+INNER JOIN funcionario ON os.id_funcionario = funcionario.id
+WHERE cliente.nome like ? || '%',
+WHERE cliente.cpf like ? || '%',
+WHERE cliente.email like ? || '%',
+WHERE cliente.telefone like ? || '%';
--- sql/criar_tabelas.sql
+++ sql/criar_tabelas.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS cliente (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     cpf TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
     telefone TEXT NOT NULL,
     senha TEXT NOT NULL
 );
@@ -27,20 +27,13 @@ CREATE TABLE IF NOT EXISTS servico (
 CREATE TABLE IF NOT EXISTS os (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_cliente INTEGER NOT NULL,
-    valort REAL NOT NULL,
+    id_servico INTEGER NOT NULL,
+    id_funcionario INTEGER NOT NULL,
+    preco_unitario REAL NOT NULL,
     data TEXT NOT NULL,
     local TEXT NOT NULL,
     descricao TEXT NOT NULL,
-    FOREIGN KEY(id_cliente) REFERENCES cliente(id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS itemos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_os INTEGER NOT NULL,
-    id_servico INTEGER NOT NULL,
-    id_funcionario INTEGER NOT NULL,
-    precoun REAL NOT NULL,
-    FOREIGN KEY (id_os) REFERENCES os(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id) ON DELETE CASCADE,
     FOREIGN KEY (id_servico) REFERENCES servico(id),
     FOREIGN KEY (id_funcionario) REFERENCES funcionario(id)
 );
